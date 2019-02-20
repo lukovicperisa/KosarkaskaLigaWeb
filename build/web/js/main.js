48b226ebb1aea3297b8ec8870b0dcb6ff48bbbfb
LUP1BG <perisa.lukovic@bosch.com>
2019-02-20 14:23:38 +0100

    File upload izmenjen

--- build/web/js/main.js
+++ build/web/js/main.js
@@ -3,9 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function (){
-   $('#seasonMenu').change(function(){
-       $('.season-selection').submit();
-   });
+$(document).ready(function () {
+    $('#slika').click(function () {
+        $('.slikaFajl').click();
+    });
+
+    function procitajURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('.slika_prikaz').attr('src', e.target.result);
+                $('.slika_prikaz').css('display', 'inline-block');
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $('.slikaFajl').change(function(){
+        procitajURL(this);
+    });
+
 });
+
+function sakrijPrikaz(){
+    $('.slika_prikaz').css('display', 'none');
+}
 
